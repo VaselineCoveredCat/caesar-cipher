7e264fc1a1cb965ad2a3905b4c1a1d284cc04ac3
Firas <babiturboe@hotmail.com>
2021-03-15 00:09:49 +0700

    add file

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+
+namespace Message_Encoder
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DecodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(textShiftAmount.Text, out int shiftAmount))
+            {
+                MessageBox.Show("Invalid input.");
+                textShiftAmount.Focus();
+            }
+            else
+            {
+                int alphabet = 26;
+                int firstLetter = 97;
+                int lastLetter = 122;
+
+                string message = txtMessage.Text.ToLower();
+                string decoded = "";
+
+                for (int i = 0; i < message.Length; i++)
+                {
+                    if (char.IsLetter(message[i]))
+                    {
+                        int newLetter = message[i];
+                        newLetter = newLetter + shiftAmount;
+
+                        if (newLetter > lastLetter)
+                        {
+                            newLetter = newLetter - alphabet;
+                        }
+                        else if (newLetter < firstLetter)
+                        {
+                            newLetter = newLetter + alphabet;
+                        }
+                        decoded = decoded + (char)newLetter;
+                    }
+                    else decoded = decoded + message[i];
+                }
+                textResult.Text = decoded;
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
